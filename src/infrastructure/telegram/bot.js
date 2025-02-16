3095421e017e271552ce09efe74b4dab5c5852ea
kika1s1 <tamiratkebede120@gmail.com>
2025-02-16 22:55:20 +0300

    api is added for productions error fixation

--- src/infrastructure/telegram/bot.js
+++ src/infrastructure/telegram/bot.js
@@ -1,5 +1,15 @@
 import TelegramBot from 'node-telegram-bot-api';
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config({})
 import { TELEGRAM_BOT_TOKEN } from '../../config/env.js';
-
+const app = express()
 export const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true });
 console.log("Bot has been started successfully");
+app.get("/cohead", (req, res)=>{
+    res.send({message:"Bot is working dude"})
+})
+
+app.listen(process.env.PORT|3000, ()=>{
+    console.log("api is working ...")
+})
